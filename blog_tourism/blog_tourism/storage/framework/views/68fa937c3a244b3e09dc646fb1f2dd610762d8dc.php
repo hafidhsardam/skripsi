91d51633188b814f8b9e6bc5a51212768e148edf
rafimp <rafi.pravidya@binus.ac.id>
2022-04-10 21:54:28 +0700

    Revert "test"
    This reverts commit 656eea32f35e91a77d02cf87378c055eaf7236d7.

--- blog_tourism/blog_tourism/storage/framework/views/68fa937c3a244b3e09dc646fb1f2dd610762d8dc.php
+++
@@ -1,6 +0,0 @@
-Coba Saja.
-<ul>
-    <?php foreach ($categorie as $row) { ?>
-        <li><?= $row->name; ?></li>
-    <?php } ?>
-</ul><?php /**PATH C:\xampp\htdocs\code_yoca\blog_tourism\resources\views/coba.blade.php ENDPATH**/ ?>
