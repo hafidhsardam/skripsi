389777625dde8443a6e80f18d2e7aa7f64a217dc
RafiWidiyanto <80093219+RafiWidiyanto@users.noreply.github.com>
2022-04-21 23:27:03 +0700

    1

--- blog_tourism/resources/views/admin/Create/CreateRFQ.blade.php
+++ blog_tourism/resources/views/admin/Create/CreateRFQ.blade.php
@@ -37,10 +37,12 @@
         <table class="table table-striped table-dark">
             <thead>
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Product</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Unit Of Measure</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Unit Of Measure</th>
+                    <th scope="col">Subtotal</th>
                 </tr>
             </thead>
             <tbody>
@@ -49,18 +51,24 @@
                     <td>Mark</td>
                     <td>Otto</td>
                     <td>@mdo</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
                 </tr>
                 <tr>
                     <th scope="row">2</th>
                     <td>Jacob</td>
                     <td>Thornton</td>
                     <td>@fat</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
                 </tr>
                 <tr>
                     <th scope="row">3</th>
                     <td>Larry</td>
                     <td>the Bird</td>
                     <td>@twitter</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
                 </tr>
             </tbody>
         </table>
