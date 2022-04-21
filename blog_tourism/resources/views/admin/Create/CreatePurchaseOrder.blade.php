0598686d31db658369d3bb9f2e468717e9f7f3df
RafiWidiyanto <80093219+RafiWidiyanto@users.noreply.github.com>
2022-04-21 23:49:10 +0700

    Revert "Revert "1""
    This reverts commit 5a6739bef0ecc9a6f0db592f48f815d2bdc59903.

--- blog_tourism/resources/views/admin/Create/CreatePurchaseOrder.blade.php
+++ blog_tourism/resources/views/admin/Create/CreatePurchaseOrder.blade.php
@@ -96,10 +96,12 @@
         <table class="table table-striped table-dark">
             <thead>
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Product</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Unit Of Measure</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Unit Of Measure</th>
+                    <th scope="col">Subtotal</th>
                 </tr>
             </thead>
             <tbody>
@@ -108,18 +110,24 @@
                     <td>Mark</td>
                     <td>Otto</td>
                     <td>@mdo</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
                 </tr>
                 <tr>
                     <th scope="row">2</th>
                     <td>Jacob</td>
                     <td>Thornton</td>
                     <td>@fat</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
                 </tr>
                 <tr>
                     <th scope="row">3</th>
                     <td>Larry</td>
                     <td>the Bird</td>
                     <td>@twitter</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
                 </tr>
             </tbody>
         </table>
