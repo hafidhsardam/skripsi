172bf9c31abb0b80c8285c95f43b214f91bca581
RafiWidiyanto <80093219+RafiWidiyanto@users.noreply.github.com>
2022-04-21 22:31:41 +0700

    1

--- blog_tourism/resources/views/admin/Create/CreateProduct.blade.php
+++ blog_tourism/resources/views/admin/Create/CreateProduct.blade.php
@@ -1,3 +1,67 @@
 @extends('admin.index')
 @section('content')
+<style>
+    .kotak {
+        position: sticky;
+        width: auto;
+        height: auto;
+        padding: 20px;
+
+        background: #FFFFFF;
+        /* 1 */
+
+        box-shadow: 0px 16px 24px rgba(0, 0, 0, 0.06), 0px 2px 6px rgba(0, 0, 0, 0.04), 0px 0px 1px rgba(0, 0, 0, 0.04);
+        border-radius: 19.0044px;
+    }
+
+    .table {
+        box-shadow: 0px 16px 24px rgba(0, 0, 0, 0.06), 0px 2px 6px rgba(0, 0, 0, 0.04), 0px 0px 1px rgba(0, 0, 0, 0.04);
+        border-radius: 19.0044px;
+    }
+    .input-group{
+        padding: 20px;
+    }
+</style>
+<div>
+    <h1>Product</h1>
+  
+</div>
+<div class='kotak'>
+    <div>
+        <a href="http://127.0.0.1:8000/CreateProduct"><button type="button" class="btn btn-dark">CREATE</button> </a>
+    </div>
+    <br>
+    <div>
+        <table class="table table-striped table-dark">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
