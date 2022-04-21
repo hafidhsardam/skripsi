2275bb6d58b3ece7b7921286e94773a963e1f77b
Hafidh Zulfiqar <72957325+hafidhzulfiqar@users.noreply.github.com>
2022-04-21 22:45:33 +0700

    1

--- blog_tourism/resources/views/admin/RFQ.blade.php
+++ blog_tourism/resources/views/admin/RFQ.blade.php
@@ -24,12 +24,6 @@
 </style>
 <div>
     <h1>Request For Quotation</h1>
-    <div class="input-group rounded">
-        <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-        <span class="input-group-text border-0" id="search-addon">
-            <i class="fas fa-search"></i>
-        </span>
-    </div>
 </div>
 <div class='kotak'>
 <table>
